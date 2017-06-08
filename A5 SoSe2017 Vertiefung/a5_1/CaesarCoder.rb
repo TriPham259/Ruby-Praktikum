33851818b4ee867ca8e279e90950fe46db838a80
Tri Pham <phamtri96@gmail.com>
2017-06-08 14:56:51 +0200

    ceasar encode

--- A5 SoSe2017 Vertiefung/a5_1/CaesarCoder.rb
+++ A5 SoSe2017 Vertiefung/a5_1/CaesarCoder.rb
@@ -1,23 +1,40 @@
 class CaesarCoder
   def initialize(original_datei,schluessel_zeichen)
-    #TODO
+    @original_datei = original_datei
+    @schluessel = schluessel_zeichen
   end
 
   #TODO
-  def encode_file()
+  def encode_file
   
   end
 
-  #TODO
-  def encode(byte,sz = @schluessel)
+  # calculate encoded letter's bytecode
+  def encode(byte,sz = @schluessel)           # given are the to be encoded bytecode and the key
+    # only letters will be encoded, numbers & special characters won't
+    if ('A'..'Z').include?(byte.chr) || ('a'..'z').include?(byte.chr)
+      start = (byte > 96) ? 'a'.ord : 'A'.ord       # determine the start-value for decoding a-z & A-Z
+      anzahl_zeichen = 26                           # das deutsche Alphabet
+      abstand = sz.ord - byte.ord                   # "displacement"
+
+      (byte + abstand - start) % anzahl_zeichen + start
+    else
+      byte
+    end
+
   end
   
   #TODO
-  def decode_file()
+  def decode_file
+
   end
+
+
 end
 
 
 cc = CaesarCoder.new("a5_1/plain.txt","C")
-cc.encode_file()
-cc.decode_file()
+cc.encode_file
+cc.decode_file
+
+puts cc.encode('a'.ord,'d'.ord)
