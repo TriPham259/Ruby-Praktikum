0689d031b3e5ce52797d18ae118fb0c565d9e8a4
Tri Pham <phamtri96@gmail.com>
2017-07-07 22:27:18 +0200

    a2 done

--- PruefungPTP12016/a2/A2Kasse.rb
+++ PruefungPTP12016/a2/A2Kasse.rb
@@ -2,6 +2,28 @@
 class Kasse
   
   #TODO 's nach Aufgabenstellung
+  include Enumerable
+  
+  def initialize
+    @rechnungen = []
+  end
+  
+  def each(&b)
+    @rechnungen.each(&b)
+  end
+  
+  def produkt_haeufigkeiten
+    result = {}
+      
+    self.each { |rechnung|
+      rechnung.each { |pos|
+        result[pos.produkt] = 0 unless result.has_key?(pos.produkt)
+        result[pos.produkt] += pos.anzahl
+      }
+    }  
+      
+    result
+  end
   
   # Gegeben
   def <<(rechnung)
@@ -34,6 +56,31 @@ class Rechnung
   end
 
   # TODO 's nach Aufgabenstellung
+  include Enumerable
+  include Comparable
+  
+  @@count = 0
+  def initialize
+    @@count += 1
+    @nr = @@count
+    @positionen = []
+  end
+  
+  def each(&b)
+    @positionen.each(&b)
+  end
+  
+  def rechnungs_betrag
+    self.inject(0) { |sum, pos| sum + pos.preis}
+  end
+  
+  def count
+    self.inject(0) { |count, pos| count + pos.anzahl}
+  end
+  
+  def <=>(other)
+    self.rechnungs_betrag <=> other.rechnungs_betrag
+  end
  
   # Gegeben
   def << pos
@@ -52,9 +99,48 @@ class Position
   attr_reader :preis,:produkt,:anzahl
 
   #TODO 's nach Aufgabenstellung
+  include Comparable
   
-  def to_s()
+  def initialize(produkt, anzahl, einzelpreis)
+    @produkt = produkt
+    @anzahl = anzahl
+    @preis = einzelpreis * anzahl
+  end
+  
+  def <=>(other)
+    [self.preis, self.produkt] <=> [other.preis, other.produkt]
+  end
+  
+  def to_s
     return "#{@produkt}(#{@anzahl}):#{preis}"
   end
 end
+
+#pos1 = Position.new('Cola',2,15)
+#pos2 = Position.new('Pepsi',3,5)
+#pos3 = Position.new('Sprite',7,1)
+#pos4 = Position.new('Pho',4,30)
+#pos5 = Position.new('Reis',1,25)
+#pos6 = Position.new('Pasta',3,25)
+#
+#rech1 = Rechnung.new
+#rech1 << pos1 << pos2 << pos3
+#rech2 = Rechnung.new
+#rech2 << pos4 << pos5 << pos6
+#
+#my_kasse = Kasse.new
+#my_kasse << rech1 << rech2
+#
+#puts rech1.to_s
+#puts rech1.rechnungs_betrag              # 52
+#puts rech1.count                         # 12
+#puts rech2.to_s
+#puts rech2.rechnungs_betrag              # 220
+#puts rech2.count                         # 8
+#
+#puts my_kasse.to_s
+#puts my_kasse.produkt_haeufigkeiten()
+#puts my_kasse.kassen_sturz()
+
+
 
