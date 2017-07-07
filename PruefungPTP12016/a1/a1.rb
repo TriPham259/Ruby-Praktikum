bf8e57afe92654a1bf8972ecd0459e615453c1d5
Tri Pham <phamtri96@gmail.com>
2017-07-07 19:37:25 +0200

    a1 done

--- PruefungPTP12016/a1/a1.rb
+++ PruefungPTP12016/a1/a1.rb
@@ -39,13 +39,16 @@ class Array
   @@dgbc_result = {}
     
   def deep_group_by_class
+    # base case
     return {} if self == []
+      
+    # recursive case
     self.each { |elem|
-      # create new "Type-Element" for the first time encountering new Type
+      # create new "Type-hash-pair" for the first time encountering a new Type
       type = elem.class
       @@dgbc_result[type] = [] unless @@dgbc_result.has_key?(type)
      
-      # add elem in value of corresponding Hash
+      # add elem in corresponding Hash-value
       @@dgbc_result[type] << elem
       
       
@@ -53,7 +56,6 @@ class Array
       elem.deep_group_by_class if elem.is_a?(Array) 
     }
     
-
     # return
     # Hash[@@dgbc_result.map {|k, v| [k.to_sym,v]}].to_s.delete(':')
     @@dgbc_result
@@ -64,15 +66,37 @@ end
 # 10 Pkt  // 8 Pkt fuer korrekte Loesung mit each
 # TODO Methode multi_value_reverse auf Hash
 def multi_value_reverse(h)
+  # check Type of parameter
+  raise ArgumentError, 'Der Parameter ist kein Hash' unless h.is_a?(Hash)
+  
+  # result is a Hash
+  result = {}
+    
+  # search through arrays of Hash-value
+  h.each { |k,v|
+    v.each { |el|
+      # create new Hash-pair when encountering new elem
+      result[el] = [] unless result.has_key?(el)
+      
+      # add the corresponding key into Hash-value 
+      result[el] << k
+    }
+  }
+  
+  result
 end
 
 #puts ln_i(2,500)
 #puts Math.log(2)
 #
 #puts [1,2].class
-nested_ary = [[[[1]]], 8, {7 => "einzeln", 5 => [13]}]
-puts nested_ary.deep_group_by_class 
-ary = []
-puts ary.deep_group_by_class
+#nested_ary = [[[[1]]], 8, {7 => "einzeln", 5 => [13]}]
+#puts nested_ary.deep_group_by_class 
+#ary = []
+#puts ary.deep_group_by_class
 
+#h = {:best1 => [:thetas, :myo, :hololens],
+#:best2 => [:gear_360, :soli, :vive] ,
+#:best3 => [:thetas, :gear_360, :myo] }
+#puts multi_value_reverse(h)
 
