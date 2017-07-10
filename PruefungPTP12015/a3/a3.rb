eff190beae6b2546b7583196dbc8158637a665cf
Tri Pham <phamtri96@gmail.com>
2017-07-10 23:21:33 +0200

    finish

--- PruefungPTP12015/a3/a3.rb
+++ PruefungPTP12015/a3/a3.rb
@@ -5,12 +5,14 @@ class Welt
   def initialize(n,m)
     @flaeche = Array.new(n){Array.new(m)}
   end
+
   # gegeben
   def each(&b)
     @flaeche.each(){|korridor|
       korridor.each(){|raum| b.call(raum)}
     }
   end
+
   # gegeben
   def to_s()
     @flaeche.map(){|korridor| korridor.join("::")}.join("\n")
@@ -18,18 +20,26 @@ class Welt
 
   # TODO setze_raum gesamt 2 Pkt
   def setze_raum(i,j,raum) #2 Pkt
+    @flaeche[i][j] = raum
   end
 
   # TODO gesamt 2Pkt
   def raum_an_position(i,j)
+    @flaeche[i][j]
   end
 
   # TODO gesamt: 7 Pkt
   def raum_position(raum)
+    @flaeche.each_with_index { |x,i|
+      x.each_with_index { |y,j|
+        return [i,j] if y == raum
+      }
+    }
   end
 
   # TODO gesamt: 4Pkt
   def wegkosten(raum1,raum2)
+    (raum_position(raum2)[0] - raum_position(raum1)[0]).abs + (raum_position(raum2)[1] - raum_position(raum1)[1]).abs
   end
 end
 
@@ -51,12 +61,23 @@ class Spieler
 
   # TODO gesamt 9 Pkt
   def optimales_ziel()
+    @welt.select { |raum|
+      erreichbare_ziele.include?(raum) && !raum.empty?
+    }.max { |a, b|
+      Float(a.wertigkeit)/gesammt_kosten(a) <=> Float(b.wertigkeit)/gesammt_kosten(b)
+    }
   end
 
   # TODO gesamt 6 Pkt
   def erreichbare_ziele()
+    @welt.select{ |raum|
+      @tragkraft >= raum.kosten && @lebenspunkte >= @welt.wegkosten(@aktueller_raum, raum) && raum != @aktueller_raum
+    }
   end
 
+  def gesammt_kosten(raum)
+    @welt.wegkosten(@aktueller_raum, raum) + raum.kosten
+  end
 end
 
 # gesamt 10 Pkt
@@ -79,24 +100,30 @@ class Raum
 
   # TODO 2 Pkt
   def <<(gegenstand)
+    @gegenstaende << gegenstand
+    self
   end
 
   # TODO 1Pkt
   def size()
+    @gegenstaende.size
   end
 
   # TODO berechnet die Summe der Gewichte der enthaltenen Gegenstaende
   # 3 Pkt
   def kosten()
+    self.inject(0){ |akku, gegenstand| akku + gegenstand.gewicht }
   end
 
   # TODO berechnet die Summe der Lebenspunkte der enthaltenen Gegestaende
   # 3 Pkt
   def wertigkeit()
+    self.inject(0){ |akku, gegenstand| akku + gegenstand.lebenspunkte }
   end
 
   # TODO 1Pkt
   def empty?()
+    @gegenstaende.empty?
   end
 
 end
@@ -112,5 +139,4 @@ class Gegenstand
     "G(#{gewicht},#{lebenspunkte})"
   end
 end
-
 
